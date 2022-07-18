6944de4c7b0ccb36c4c9d2d852d3d6197e4d8ea1
Dariusz Depta <dariusz.depta@dmntk.io>
2022-07-18 21:00:52 +0200

    Added more unit tests.

--- common/src/errors.rs
+++ common/src/errors.rs
@@ -75,11 +75,19 @@ mod tests {
 
   #[test]
   fn test_equal() {
-    assert!((DmntkError::new("ParserError", "unexpected end of file") == DmntkError::new("ParserError", "unexpected end of file")));
+    let err1 = DmntkError::new("ParserError", "unexpected end of file");
+    let err2 = DmntkError::new("ParserError", "unexpected end of file");
+    assert!((err1 == err2));
   }
 
   #[test]
   fn test_not_equal() {
     assert!((DmntkError::new("ParserError", "unexpected end of files") != DmntkError::new("ParserError", "unexpected end of file")));
+  }
+
+  #[test]
+  fn test_total_eq() {
+    let err1 = DmntkError::new("ParserError", "unexpected end of file");
+    err1.assert_receiver_is_total_eq();
   }
 }
