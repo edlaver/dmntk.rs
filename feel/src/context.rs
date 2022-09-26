99bdb5536e0cee64895e042b898e1517fda3bf00
Dariusz Depta <dariusz.depta@dmntk.io>
2022-09-26 16:31:57 +0200

    Refactoring & code-coverage.

--- feel/src/context.rs
+++ feel/src/context.rs
@@ -65,7 +65,7 @@ impl TryFrom<Value> for FeelContext {
     if let Value::Context(ctx) = value {
       Ok(ctx)
     } else {
-      Err(value_is_not_a_context(&value))
+      Err(err_value_is_not_a_context(&value))
     }
   }
 }
@@ -208,17 +208,17 @@ impl FeelContext {
             if !sub_keys.is_empty() {
               for sub_key in sub_keys {
                 keys.insert(sub_key.clone());
-                keys.insert(format!("{} . {}", key, sub_key)); //TODO add test for this case, nothing happened after adding spaces
+                keys.insert(format!("{} . {}", key, sub_key));
               }
             }
           }
         }
       }
-      if let Value::FeelType(FeelType::Context(a)) = value {
-        for name in a.keys() {
+      if let Value::FeelType(FeelType::Context(type_entries)) = value {
+        for name in type_entries.keys() {
           let sub_key = name.to_string();
           keys.insert(sub_key.clone());
-          keys.insert(format!("{} . {}", key, sub_key)); //TODO add test for this case, nothing happened after adding spaces
+          keys.insert(format!("{} . {}", key, sub_key));
         }
       }
     }
@@ -287,410 +287,21 @@ impl FeelContext {
 }
 
 /// Definitions of context errors.
-pub mod errors {
+mod errors {
   use crate::values::Value;
   use dmntk_common::DmntkError;
 
   /// Context errors.
-  #[derive(Debug, PartialEq)]
-  enum ContextError {
-    /// Used when converting a [Value] to [FeelContext](super::FeelContext).
-    ValueIsNotAContext(String),
-  }
+  struct ContextError(String);
 
   impl From<ContextError> for DmntkError {
     fn from(e: ContextError) -> Self {
-      DmntkError::new("ContextError", &format!("{}", e))
+      DmntkError::new("ContextError", &e.0)
     }
   }
 
-  impl std::fmt::Display for ContextError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-      match self {
-        ContextError::ValueIsNotAContext(text) => {
-          write!(f, "'{}' is not a value containing context", text)
-        }
-      }
-    }
-  }
-
-  pub fn value_is_not_a_context(value: &Value) -> DmntkError {
-    ContextError::ValueIsNotAContext(value.to_string()).into()
-  }
-}
-
-#[cfg(test)]
-mod tests {
-  use crate::context::FeelContext;
-  use crate::names::Name;
-  use crate::qualified_names::QualifiedName;
-  use crate::values::Value;
-  use crate::{value_number, ToFeelString};
-  use dmntk_common::Jsonify;
-
-  #[test]
-  fn test_context_default() {
-    let ctx: FeelContext = Default::default();
-    assert_eq!("{}", ctx.to_string());
-    assert!(ctx.flatten_keys().is_empty());
-  }
-
-  #[test]
-  fn test_context_to_string() {
-    let name_a = Name::from("a");
-    let name_x_y = Name::new(&["x", "y"]);
-    let name_k_plus_l_minus_m = Name::new(&["k", "+", "l", "-", "m"]);
-    let mut ctx: FeelContext = Default::default();
-    ctx.set_entry(&name_a, value_number!(10));
-    assert_eq!(r#"{a: 10}"#, ctx.to_string());
-    ctx.set_entry(&name_x_y, Value::Boolean(true));
-    assert_eq!(r#"{a: 10, x y: true}"#, ctx.to_string());
-    ctx.set_entry(&name_k_plus_l_minus_m, Value::String("KLM".to_string()));
-    assert_eq!(r#"{a: 10, k+l-m: "KLM", x y: true}"#, ctx.to_string());
-  }
-
-  #[test]
-  fn test_context_to_feel_string() {
-    let name_a = Name::from("a");
-    let name_x_y = Name::new(&["x", "y"]);
-    let name_k_plus_l_minus_m = Name::new(&["k", "+", "l", "-", "m"]);
-    let mut ctx: FeelContext = Default::default();
-    ctx.set_entry(&name_a, value_number!(10));
-    assert_eq!(r#"{a: 10}"#, ctx.to_feel_string());
-    ctx.set_entry(&name_x_y, Value::Boolean(true));
-    assert_eq!(r#"{a: 10, x y: true}"#, ctx.to_feel_string());
-    ctx.set_entry(&name_k_plus_l_minus_m, Value::String("KLM".to_string()));
-    assert_eq!(r#"{a: 10, k+l-m: "KLM", x y: true}"#, ctx.to_feel_string());
-    let mut ctx: FeelContext = Default::default();
-    let name_left_bracket = Name::from("{");
-    ctx.set_entry(&name_left_bracket, value_number!(1));
-    assert_eq!(r#"{"{": 1}"#, ctx.to_feel_string());
-    let name_right_bracket = Name::from("}");
-    ctx.set_entry(&name_right_bracket, value_number!(2));
-    assert_eq!(r#"{"{": 1, "}": 2}"#, ctx.to_feel_string());
-    let name_colon = Name::from(":");
-    ctx.set_entry(&name_colon, value_number!(3));
-    assert_eq!(r#"{":": 3, "{": 1, "}": 2}"#, ctx.to_feel_string());
-    let name_comma = Name::from(",");
-    ctx.set_entry(&name_comma, value_number!(4));
-    assert_eq!(r#"{",": 4, ":": 3, "{": 1, "}": 2}"#, ctx.to_feel_string());
-    let name_double_quote = Name::from("\"");
-    ctx.set_entry(&name_double_quote, value_number!(5));
-    assert_eq!(r#"{"\"": 5, ",": 4, ":": 3, "{": 1, "}": 2}"#, ctx.to_feel_string());
-  }
-
-  #[test]
-  fn test_context_to_json() {
-    let name_a = Name::from("a");
-    let name_x_y = Name::new(&["x", "y"]);
-    let name_k_plus_l_minus_m = Name::new(&["k", "+", "l", "-", "m"]);
-    let mut ctx: FeelContext = Default::default();
-    ctx.set_entry(&name_a, value_number!(10));
-    assert_eq!(r#"{"a": 10}"#, ctx.jsonify());
-    ctx.set_entry(&name_x_y, Value::Boolean(true));
-    assert_eq!(r#"{"a": 10, "x y": true}"#, ctx.jsonify());
-    ctx.set_entry(&name_k_plus_l_minus_m, Value::String("KLM".to_string()));
-    assert_eq!(r#"{"a": 10, "k+l-m": "KLM", "x y": true}"#, ctx.jsonify());
-  }
-
-  #[test]
-  fn test_context_one_level() {
-    let name_a = Name::from("a");
-    let name_a_b = Name::new(&["a", "b"]);
-    let name_a_b_c = Name::new(&["a", "b", "c"]);
-    let qname_a = QualifiedName::new(&[&name_a]);
-    let qname_a_b = QualifiedName::new(&[&name_a_b]);
-    let qname_a_b_c = QualifiedName::new(&[&name_a_b_c]);
-    let mut ctx_a: FeelContext = Default::default();
-    ctx_a.set_entry(&name_a, value_number!(10));
-    assert_eq!("{a: 10}", ctx_a.to_string());
-    assert!(ctx_a.contains_entry(&name_a));
-    assert!(ctx_a.contains_entries(&qname_a));
-    assert!(!ctx_a.contains_entry(&name_a_b));
-    assert!(!ctx_a.contains_entries(&qname_a_b));
-    assert!(!ctx_a.contains_entry(&name_a_b_c));
-    assert!(!ctx_a.contains_entries(&qname_a_b_c));
-    assert!(ctx_a.flatten_keys().contains("a"));
-    assert_eq!("10", ctx_a.get_entry(&name_a).unwrap().to_string().as_str());
-  }
-
-  #[test]
-  fn test_context_two_levels() {
-    let name_married = Name::from("married");
-    let name_age = Name::from("age");
-    let name_b = Name::from("b");
-    let name_x_y = Name::new(&["x", "y"]);
-    let qname_married = QualifiedName::new(&[&name_married]);
-    let qname_age = QualifiedName::new(&[&name_age]);
-    let qname_b = QualifiedName::new(&[&name_b]);
-    let qname_b_married = QualifiedName::new(&[&name_b, &name_married]);
-    let qname_b_married_age = QualifiedName::new(&[&name_b, &name_married, &name_age]);
-    let mut ctx_b: FeelContext = Default::default();
-    ctx_b.set_entry(&name_married, Value::Boolean(true));
-    assert_eq!("{married: true}", ctx_b.to_string());
-    assert!(ctx_b.contains_entry(&name_married));
-    assert!(ctx_b.contains_entries(&qname_married));
-    let mut ctx_a: FeelContext = Default::default();
-    ctx_a.set_entry(&name_age, value_number!(49));
-    ctx_a.set_entry(&name_x_y, Value::Boolean(true));
-    ctx_a.set_entry(&name_b, ctx_b.into());
-    assert_eq!("{age: 49, b: {married: true}, x y: true}", ctx_a.to_string());
-    assert!(ctx_a.contains_entry(&name_age));
-    assert!(ctx_a.contains_entry(&name_b));
-    assert!(ctx_a.contains_entry(&name_x_y));
-    assert!(ctx_a.contains_entries(&qname_age));
-    assert!(ctx_a.contains_entries(&qname_b));
-    assert!(ctx_a.contains_entries(&qname_b_married));
-    assert!(!ctx_a.contains_entries(&qname_b_married_age));
-    assert!(ctx_a.flatten_keys().contains("age"));
-    assert!(ctx_a.flatten_keys().contains("b"));
-    assert!(ctx_a.flatten_keys().contains("b . married"));
-    assert_eq!("49", ctx_a.get_entry(&name_age).unwrap().to_string().as_str());
-    assert_eq!("{married: true}", ctx_a.get_entry(&name_b).unwrap().to_string().as_str());
-  }
-
-  #[test]
-  fn test_context_three_levels() {
-    let name_car = Name::from("car");
-    let name_married = Name::from("married");
-    let name_age = Name::from("age");
-    let name_b = Name::from("b");
-    let name_c = Name::from("c");
-    let mut ctx_c: FeelContext = Default::default();
-    ctx_c.set_entry(&name_car, Value::String("opel".to_string()));
-    assert_eq!(r#"{car: "opel"}"#, ctx_c.to_string());
-    assert!(ctx_c.contains_entry(&name_car));
-    let mut ctx_b: FeelContext = Default::default();
-    ctx_b.set_entry(&name_married, Value::Boolean(true));
-    ctx_b.set_entry(&name_c, ctx_c.into());
-    assert_eq!(r#"{c: {car: "opel"}, married: true}"#, ctx_b.to_string());
-    assert!(ctx_b.contains_entry(&name_married));
-    assert!(ctx_b.contains_entry(&name_c));
-    let mut ctx_a: FeelContext = Default::default();
-    ctx_a.set_entry(&name_age, value_number!(49));
-    ctx_a.set_entry(&name_b, ctx_b.into());
-    assert_eq!(r#"{age: 49, b: {c: {car: "opel"}, married: true}}"#, ctx_a.to_string());
-    assert!(ctx_a.contains_entry(&name_age));
-    assert!(ctx_a.contains_entry(&name_b));
-    assert!(ctx_a.flatten_keys().contains("age"));
-    assert!(ctx_a.flatten_keys().contains("b"));
-    assert!(ctx_a.flatten_keys().contains("b . c"));
-    assert!(ctx_a.flatten_keys().contains("b . married"));
-    assert!(ctx_a.flatten_keys().contains("b . c . car"));
-  }
-
-  #[test]
-  fn test_context_search_entry() {
-    let name_married = Name::from("married");
-    let name_age = Name::from("age");
-    let name_b = Name::from("b");
-    let mut ctx_b: FeelContext = Default::default();
-    ctx_b.set_entry(&name_married, Value::Boolean(true));
-    let mut ctx_a: FeelContext = Default::default();
-    ctx_a.set_entry(&name_age, value_number!(49));
-    ctx_a.set_entry(&name_b, ctx_b.into());
-    let qn_empty = QualifiedName::new(&[]);
-    assert!(ctx_a.search_entry(&qn_empty).is_none());
-    let qn_b = QualifiedName::new(&[&name_b]);
-    assert_eq!("{married: true}", ctx_a.search_entry(&qn_b).unwrap().to_string().as_str());
-    let qn_b_married = QualifiedName::new(&[&name_b, &name_married]);
-    assert_eq!("true", ctx_a.search_entry(&qn_b_married).unwrap().to_string().as_str());
-  }
-
-  #[test]
-  fn test_context_search_entries() {
-    // prepare names
-    let name_a = Name::from("a");
-    let name_b = Name::from("b");
-    let name_c = Name::from("c");
-    let name_d = Name::from("d");
-    let name_e = Name::from("e");
-    // prepare qualified names
-    let qn_a = QualifiedName::new(&[&name_a]);
-    let qn_b = QualifiedName::new(&[&name_b]);
-    let qn_c = QualifiedName::new(&[&name_c]);
-    let qn_a_b = QualifiedName::new(&[&name_a, &name_b]);
-    let qn_a_c = QualifiedName::new(&[&name_a, &name_c]);
-    let qn_b_c = QualifiedName::new(&[&name_b, &name_c]);
-    let qn_b_e = QualifiedName::new(&[&name_b, &name_e]);
-    let qn_b_d = QualifiedName::new(&[&name_b, &name_d]);
-    let qn_b_d_a = QualifiedName::new(&[&name_b, &name_d, &name_a]);
-    let qn_b_d_e = QualifiedName::new(&[&name_b, &name_d, &name_e]);
-    // prepare contexts
-    let mut ctx_c: FeelContext = Default::default();
-    ctx_c.set_entry(&name_e, Value::String("e".to_string()));
-    assert_eq!(r#"{e: "e"}"#, ctx_c.to_string());
-    let mut ctx_b: FeelContext = Default::default();
-    ctx_b.set_entry(&name_c, Value::String("c".to_string()));
-    ctx_b.set_entry(&name_d, ctx_c.into());
-    assert_eq!(r#"{c: "c", d: {e: "e"}}"#, ctx_b.to_string());
-    let mut ctx_a: FeelContext = Default::default();
-    ctx_a.set_entry(&name_a, Value::String("a".to_string()));
-    ctx_a.set_entry(&name_b, ctx_b.into());
-    assert_eq!(r#"{a: "a", b: {c: "c", d: {e: "e"}}}"#, ctx_a.to_string());
-    // test searching entries
-    assert!(ctx_a.contains_entries(&qn_a));
-    assert!(ctx_a.contains_entries(&qn_b));
-    assert!(!ctx_a.contains_entries(&qn_c));
-    assert!(!ctx_a.contains_entries(&qn_a_b));
-    assert!(!ctx_a.contains_entries(&qn_a_c));
-    assert!(ctx_a.contains_entries(&qn_b_c));
-    assert!(!ctx_a.contains_entries(&qn_b_e));
-    assert!(ctx_a.contains_entries(&qn_b_d));
-    assert!(!ctx_a.contains_entries(&qn_b_d_a));
-    assert!(ctx_a.contains_entries(&qn_b_d_e));
-  }
-
-  #[test]
-  fn test_context_create_entry() {
-    let name_a = Name::from("a");
-    let name_b = Name::from("b");
-    let name_c = Name::from("c");
-    let name_d = Name::from("d");
-    let qn_a = QualifiedName::new(&[&name_a]);
-    let qn_b = QualifiedName::new(&[&name_b]);
-    let qn_a_b = QualifiedName::new(&[&name_a, &name_b]);
-    let qn_a_c = QualifiedName::new(&[&name_a, &name_c]);
-    let qn_a_d = QualifiedName::new(&[&name_a, &name_d]);
-    let qn_c_d = QualifiedName::new(&[&name_c, &name_d]);
-    let qn_a_b_c = QualifiedName::new(&[&name_a, &name_b, &name_c]);
-    let qn_a_b_c_d = QualifiedName::new(&[&name_a, &name_b, &name_c, &name_d]);
-    let mut ctx: FeelContext = Default::default();
-    ctx.create_entry(&qn_a_b_c_d, Value::Boolean(true));
-    assert_eq!("{a: {b: {c: {d: true}}}}", ctx.to_string().as_str());
-    assert_eq!("{b: {c: {d: true}}}", ctx.search_entry(&qn_a).unwrap().to_string().as_str());
-    assert_eq!("{c: {d: true}}", ctx.search_entry(&qn_a_b).unwrap().to_string().as_str());
-    assert_eq!("{d: true}", ctx.search_entry(&qn_a_b_c).unwrap().to_string().as_str());
-    assert_eq!("true", ctx.search_entry(&qn_a_b_c_d).unwrap().to_string().as_str());
-    let mut ctx: FeelContext = Default::default();
-    ctx.create_entry(&qn_a, Value::Boolean(true));
-    ctx.create_entry(&qn_b, Value::Boolean(false));
-    ctx.create_entry(&qn_c_d, Value::String("deep".to_string()));
-    assert_eq!(r#"{a: true, b: false, c: {d: "deep"}}"#, ctx.to_string().as_str());
-    let mut ctx: FeelContext = Default::default();
-    ctx.create_entry(&qn_a_b, Value::String("b".to_string()));
-    ctx.create_entry(&qn_a_c, Value::String("c".to_string()));
-    ctx.create_entry(&qn_a_d, Value::String("d".to_string()));
-    assert_eq!(r#"{a: {b: "b", c: "c", d: "d"}}"#, ctx.to_string().as_str());
-  }
-
-  #[test]
-  fn test_context_flatten_keys_one_level() {
-    let name_a = Name::from("a");
-    let name_b = Name::from("b");
-    let mut ctx: FeelContext = Default::default();
-    ctx.set_entry(&name_a, value_number!(1));
-    ctx.set_entry(&name_b, value_number!(2));
-    assert_eq!(r#"{a: 1, b: 2}"#, ctx.to_string());
-    let keys = ctx.flatten_keys();
-    assert_eq!(2, keys.len());
-    assert!(keys.contains("a"));
-    assert!(keys.contains("b"));
-  }
-
-  #[test]
-  fn test_flatten_two_levels() {
-    let name_a = Name::from("a");
-    let name_b = Name::from("b");
-    let name_c = Name::from("c");
-    let mut ctx_b: FeelContext = Default::default();
-    ctx_b.set_entry(&name_a, value_number!(10));
-    ctx_b.set_entry(&name_b, value_number!(20));
-    ctx_b.set_entry(&name_c, value_number!(30));
-    let mut ctx_a: FeelContext = Default::default();
-    ctx_a.set_entry(&name_a, value_number!(1));
-    ctx_a.set_entry(&name_b, value_number!(2));
-    ctx_a.set_entry(&name_c, ctx_b.into());
-    let keys = ctx_a.flatten_keys();
-    assert_eq!(6, keys.len());
-    assert!(keys.contains("a"));
-    assert!(keys.contains("b"));
-    assert!(keys.contains("c"));
-    assert!(keys.contains("c . a"));
-    assert!(keys.contains("c . b"));
-    assert!(keys.contains("c . c"));
-  }
-
-  #[test]
-  fn test_flatten_three_levels() {
-    let name_a = Name::from("a");
-    let name_b = Name::from("b");
-    let name_c = Name::from("c");
-    let name_d = Name::from("d");
-    let mut ctx_c: FeelContext = Default::default();
-    ctx_c.set_entry(&name_a, value_number!(100));
-    ctx_c.set_entry(&name_b, value_number!(200));
-    ctx_c.set_entry(&name_c, value_number!(300));
-    ctx_c.set_entry(&name_d, value_number!(400));
-    let mut ctx_b: FeelContext = Default::default();
-    ctx_b.set_entry(&name_a, value_number!(10));
-    ctx_b.set_entry(&name_b, value_number!(20));
-    ctx_b.set_entry(&name_c, value_number!(30));
-    ctx_b.set_entry(&name_d, ctx_c.into());
-    let mut ctx_a: FeelContext = Default::default();
-    ctx_a.set_entry(&name_a, value_number!(1));
-    ctx_a.set_entry(&name_b, value_number!(2));
-    ctx_a.set_entry(&name_c, ctx_b.into());
-    let keys = ctx_a.flatten_keys();
-    assert_eq!(16, keys.len());
-    assert!(keys.contains("a"));
-    assert!(keys.contains("b"));
-    assert!(keys.contains("c"));
-    assert!(keys.contains("d"));
-    assert!(keys.contains("c . a"));
-    assert!(keys.contains("c . b"));
-    assert!(keys.contains("c . c"));
-    assert!(keys.contains("c . d"));
-    assert!(keys.contains("c . d . a"));
-    assert!(keys.contains("c . d . b"));
-    assert!(keys.contains("c . d . c"));
-    assert!(keys.contains("c . d . d"));
-    assert!(keys.contains("d . a"));
-    assert!(keys.contains("d . b"));
-    assert!(keys.contains("d . c"));
-    assert!(keys.contains("d . d"));
-  }
-
-  #[test]
-  fn test_flatten_names_with_additional_characters() {
-    let name_a = Name::new(&["lorem", "ipsum", "dolor", "sit", "amet"]);
-    let name_b = Name::new(&["b"]);
-    let name_c = Name::new(&["now", "let", "'", "s", "go", "to", "the", "next", "paragraph"]);
-    let name_d = Name::new(&["Curabitur", "rhoncus", "+", "sodales", "odio", "in", "fringilla"]);
-    let mut ctx_b: FeelContext = Default::default();
-    ctx_b.set_entry(&name_d, Value::Boolean(false));
-    let mut ctx_a: FeelContext = Default::default();
-    ctx_a.set_entry(&name_a, value_number!(1));
-    ctx_a.set_entry(&name_b, value_number!(2));
-    ctx_a.set_entry(&name_c, ctx_b.into());
-    let keys = ctx_a.flatten_keys();
-    assert_eq!(5, keys.len());
-    assert!(keys.contains("b"));
-    assert!(keys.contains("lorem ipsum dolor sit amet"));
-    assert!(keys.contains("now let's go to the next paragraph"));
-    assert!(keys.contains("now let's go to the next paragraph . Curabitur rhoncus+sodales odio in fringilla"));
-    assert!(keys.contains("Curabitur rhoncus+sodales odio in fringilla"));
-  }
-
-  #[test]
-  fn test_context_set_null() {
-    let name_a = Name::from("a");
-    let mut ctx: FeelContext = Default::default();
-    ctx.set_null(name_a);
-    assert_eq!(r#"{a: null}"#, ctx.to_string());
-  }
-
-  #[test]
-  fn test_get_entries() {
-    let mut ctx: FeelContext = Default::default();
-    assert_eq!(0, ctx.get_entries().len());
-    let name_a = Name::new(&["a"]);
-    ctx.set_entry(&name_a, value_number!(1));
-    assert_eq!(1, ctx.get_entries().len());
-    assert_eq!(vec![(&name_a, &value_number!(1))], ctx.get_entries());
-    let name_b = Name::new(&["b"]);
-    ctx.set_entry(&name_b, value_number!(2));
-    assert_eq!(2, ctx.get_entries().len());
-    assert_eq!(vec![(&name_a, &value_number!(1)), (&name_b, &value_number!(2))], ctx.get_entries());
+  /// Creates an instance of `value is not a context` error.
+  pub fn err_value_is_not_a_context(value: &Value) -> DmntkError {
+    ContextError(format!("'{}' is not a value containing context", value)).into()
   }
 }
