599c90c134a4ba002cfa24998f3e13bc36d7631a
Dariusz Depta <dariusz.depta@dmntk.io>
2023-02-06 09:41:28 +0100

    Added tests.

--- feel-evaluator/src/tests/context.rs
+++ feel-evaluator/src/tests/context.rs
@@ -61,7 +61,7 @@ fn _0002() {
 
 #[test]
 fn _0003() {
-  let node = AstNode::Context(vec![AstNode::String("a".to_string())]);
+  let node = AstNode::Context(vec![AstNode::String("key".to_string())]);
   let result = crate::evaluate(&scope!(), &node);
   assert_eq!(r#"null(expected context entry, actual value type is string)"#, result.ok().unwrap().to_string());
 }
@@ -69,7 +69,7 @@ fn _0003() {
 #[test]
 fn _0004() {
   let node = AstNode::Context(vec![AstNode::ContextEntry(
-    Box::new(AstNode::String("a".to_string())),
+    Box::new(AstNode::String("key".to_string())),
     Box::new(AstNode::String("value".to_string())),
   )]);
   let result = crate::evaluate(&scope!(), &node);
@@ -82,4 +82,21 @@ fn _0005() {
   let node = dmntk_feel_parser::parse_context(&scope!(), input, false).unwrap();
   let result = crate::evaluate(&scope!(), &node);
   assert_eq!(r#"{convert: null(expected type of the formal parameter)}"#, result.ok().unwrap().to_string());
+}
+
+#[test]
+fn _0006() {
+  let node = AstNode::ContextType(vec![AstNode::ContextTypeEntry(
+    Box::new(AstNode::String("key".to_string())),
+    Box::new(AstNode::String("value".to_string())),
+  )]);
+  let result = crate::evaluate(&scope!(), &node);
+  assert_eq!(r#"null(expected a name in context type entry)"#, result.ok().unwrap().to_string());
+}
+
+#[test]
+fn _0007() {
+  let node = AstNode::ContextType(vec![AstNode::String("key".to_string())]);
+  let result = crate::evaluate(&scope!(), &node);
+  assert_eq!(r#"null(expected context type entry, actual value type is string)"#, result.ok().unwrap().to_string());
 }
