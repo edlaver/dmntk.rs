76d02e7a1a73c7fc7f9397ea5f71ba2ba9cb5af9
Dariusz Depta <dariusz.depta@dmntk.io>
2023-02-07 15:00:45 +0100

    Added tests.

--- feel-evaluator/src/tests/out_operator.rs
+++ feel-evaluator/src/tests/out_operator.rs
@@ -42,3 +42,10 @@ fn _0001() {
   let result = crate::evaluate(&scope!(), &node);
   assert_eq!(r#"21"#, result.ok().unwrap().to_string());
 }
+
+#[test]
+fn _0002() {
+  let node = AstNode::Out(Box::new(AstNode::Numeric("21".to_string(), "".to_string())), Box::new(AstNode::FormalParameters(vec![])));
+  let result = crate::evaluate(&scope!(), &node);
+  assert_eq!(r#"null"#, result.ok().unwrap().to_string());
+}
