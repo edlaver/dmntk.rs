0df6bc9482f4c015ce3a83012e68c909de4cb339
Dariusz Depta <dariusz.depta@dmntk.io>
2023-02-10 17:42:44 +0100

    Fixed benchamrks.

--- model-evaluator/benches/compatibility/dmn_2_0003.rs
+++ model-evaluator/benches/compatibility/dmn_2_0003.rs
@@ -52,6 +52,6 @@ fn _0001(b: &mut Bencher) {
 fn _0002(b: &mut Bencher) {
   let ctx = context(r#"{Employment Status: "RETIRED"}"#);
   let invocable_name = "Employment Status Statement";
-  assert_decision(&MODEL_EVALUATOR, invocable_name, &ctx, r#"null(addition err 2)"#);
+  assert_decision(&MODEL_EVALUATOR, invocable_name, &ctx, r#"null(expected string as a second argument in addition)"#);
   b.iter(|| MODEL_EVALUATOR.evaluate_invocable(invocable_name, &ctx));
 }
