dc33a2308666884611e573fece4a5b456597b9ea
Dariusz Depta <dariusz.depta@dmntk.io>
2023-05-17 18:42:17 +0200

    Added tests.

--- bbt/tests/server/evaluate_tck/no_namespace_name/run.sh
+++ bbt/tests/server/evaluate_tck/no_namespace_name/run.sh
@@ -19,19 +19,11 @@ curl -s -d '@data.json' -H "Content-Type: application/json" -X POST http://0.0.0
 # delete data file
 rm data.json
 
-# prepare JSON data file containing a model to be added to workspace
-echo -n '{"content":"' > data.json
-base64 --wrap=0 2_0001_a.dmn >> data.json
-echo -n '"}' >> data.json
+# deploy model
+curl -s -X POST http://0.0.0.0:22022/definitions/deploy > /dev/null 2>&1
 
-# add model to workspace
-curl -s -d '@data.json' -H "Content-Type: application/json" -X POST http://0.0.0.0:22022/definitions/add > /dev/null 2>&1
-
-# delete data file
-rm data.json
-
-# deploy models
-curl -s -X POST http://0.0.0.0:22022/definitions/deploy
+# evaluate model using generic input data format
+curl -s -d '{"model":"compliance-level-2-test-0001","invocable":"Greeting Message","input":[{"name":"Full Name","value":{"simple":{"type":"xsd:string","text":"John Doe","isNil":false}}}]}' -H "Content-Type: application/json" -X POST http://0.0.0.0:22022/tck/evaluate
 
 # stop dmntk server
 kill -s SIGINT "$_pid"
