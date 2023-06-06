422ddd22bb9dfdd6407d280ad9fcf73f5c752ccd
Dariusz Depta <dariusz.depta@dmntk.io>
2023-06-06 15:51:54 +0200

    Refactoring.

--- examples/src/compatibility/mod.rs
+++ examples/src/compatibility/mod.rs
@@ -36,35 +36,9 @@
 //! [DMN™ Technology Compatibility Kit](https://dmn-tck.github.io/tck/) project.
 //! The content of each test file is modified and differs from the original.
 
-// compatibility level 2
-pub const DMN_2_0001: &str = include_str!("level_2/2_0001.dmn");
-pub const DMN_2_0002: &str = include_str!("level_2/2_0002.dmn");
-pub const DMN_2_0003: &str = include_str!("level_2/2_0003.dmn");
-pub const DMN_2_0004: &str = include_str!("level_2/2_0004.dmn");
-pub const DMN_2_0005: &str = include_str!("level_2/2_0005.dmn");
-pub const DMN_2_0006: &str = include_str!("level_2/2_0006.dmn");
-pub const DMN_2_0007: &str = include_str!("level_2/2_0007.dmn");
-pub const DMN_2_0008: &str = include_str!("level_2/2_0008.dmn");
-pub const DMN_2_0009: &str = include_str!("level_2/2_0009.dmn");
-pub const DMN_2_0010: &str = include_str!("level_2/2_0010.dmn");
-pub const DMN_2_0100: &str = include_str!("level_2/2_0100.dmn");
-pub const DMN_2_0101: &str = include_str!("level_2/2_0101.dmn");
-pub const DMN_2_0102: &str = include_str!("level_2/2_0102.dmn");
-pub const DMN_2_0105: &str = include_str!("level_2/2_0105.dmn");
-pub const DMN_2_0106: &str = include_str!("level_2/2_0106.dmn");
-pub const DMN_2_0107: &str = include_str!("level_2/2_0107.dmn");
-pub const DMN_2_0108: &str = include_str!("level_2/2_0108.dmn");
-pub const DMN_2_0109: &str = include_str!("level_2/2_0109.dmn");
-pub const DMN_2_0110: &str = include_str!("level_2/2_0110.dmn");
-pub const DMN_2_0111: &str = include_str!("level_2/2_0111.dmn");
-pub const DMN_2_0112: &str = include_str!("level_2/2_0112.dmn");
-pub const DMN_2_0113: &str = include_str!("level_2/2_0113.dmn");
-pub const DMN_2_0114: &str = include_str!("level_2/2_0114.dmn");
-pub const DMN_2_0115: &str = include_str!("level_2/2_0115.dmn");
-pub const DMN_2_0116: &str = include_str!("level_2/2_0116.dmn");
-pub const DMN_2_0117: &str = include_str!("level_2/2_0117.dmn");
-pub const DMN_2_0118: &str = include_str!("level_2/2_0118.dmn");
-pub const DMN_2_0119: &str = include_str!("level_2/2_0119.dmn");
+mod level_2;
+
+pub use level_2::*;
 
 // compatibility level 3
 pub const DMN_3_0001: &str = include_str!("level_3/3_0001.dmn");
